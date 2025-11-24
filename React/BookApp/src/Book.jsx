745e5faa5f253f904fd91f0b0b7a1ce6fe5fd3b3
Agrim027 <agrimg8@gmail.com>
2025-11-24 12:52:01 +0530

    React

--- React/BookApp/src/Book.jsx
+++ React/BookApp/src/Book.jsx
@@ -5,25 +5,25 @@ function Book(){
   return(
     <div id="line">
       <div id="book">
-      <img src="" alt="" height={100} width={100}/>
-      <h1>TITLE:Maths</h1>
-      <h1>250</h1>
-      <button>+</button>
-      <button>-</button>
+        <img src="" alt="" height={100} width={100}/>
+        <h1>TITLE:Maths</h1>
+        <h1>250</h1>
+        <button>+</button>
+        <button>-</button>
       </div>
       <div id="book"> 
-      <img src="" alt="" height={100} width={100}/>
-      <h1>TITLE:Maths</h1>
-      <h1>250</h1>
-      <button>+</button>
-      <button>-</button>
+        <img src="" alt="" height={100} width={100}/>
+        <h1>TITLE:Maths</h1>
+        <h1>250</h1>
+        <button>+</button>
+        <button>-</button>
       </div>
       <div id="book">
-      <img src="" alt="" height={100} width={100}/>
-      <h1>TITLE:Maths</h1>
-      <h1>250</h1>
-      <button>+</button>
-      <button>-</button>
+        <img src="" alt="" height={100} width={100}/>
+        <h1>TITLE:Maths</h1>
+        <h1>250</h1>
+        <button>+</button>
+        <button>-</button>
       </div>
     </div>
   )
