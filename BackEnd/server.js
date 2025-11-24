15e96b83db9f75ce242bdb27f9a9974663e74c40
Agrim027 <agrimg8@gmail.com>
2025-11-24 12:17:53 +0530

    BackEnd Day01

---
+++ BackEnd/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express()
+const port = 8080;
+
+// app.get('/',(req , res)=>{
+//     res.send(`<h1>hi this is my first server</h1>`);
+// });
+
+app.get('/about',(req , res)=>{
+    res.send(`hi this is my first About`);
+});
+
+app.get('/contact',(req , res)=>{
+    res.send(`hi this is My Contact`);
+});
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running at 8080: http://localhost:${port}`);
+})
